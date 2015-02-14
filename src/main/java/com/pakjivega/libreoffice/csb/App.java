6e06341bb89effd417f94fd5a497fbaf0066192b
Jorge de la Vega <pakjivega@gmail.com>
2015-02-14 01:24:37 +0000

    first commit

---
+++ src/main/java/com/pakjivega/libreoffice/csb/App.java
@@ -0,0 +1,13 @@
+package com.pakjivega.libreoffice.csb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
