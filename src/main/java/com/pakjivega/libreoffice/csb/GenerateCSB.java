8e9495044b2a5e3613f00a21bc8f06f9827370cc
Jorge de la Vega <pakjivega@gmail.com>
2015-02-15 02:29:55 +0000

    clean some unused code

--- src/main/java/com/pakjivega/libreoffice/csb/GenerateCSB.java
+++ src/main/java/com/pakjivega/libreoffice/csb/GenerateCSB.java
@@ -13,60 +13,19 @@ import com.sun.star.text.XText;
 public class GenerateCSB {
 	// private static XSpreadsheet maSheet;
 	public static void generateCSB19(XScriptContext xSc) {
-
 		// getting the text document object
 		XSpreadsheetDocument xsheetdocument = (XSpreadsheetDocument) UnoRuntime.queryInterface(XSpreadsheetDocument.class, xSc.getDocument());
 		XSpreadsheets xSpread = xsheetdocument.getSheets();
-		//String[] names = xSpread.getElementNames();
-		//xSpread.insertNewByName("MySheet", (short) 0);
+		
 		try {
 			XIndexAccess aSheetsIA = UnoRuntime.queryInterface(XIndexAccess.class, xSpread);
 			XSpreadsheet maSheet = UnoRuntime.queryInterface(XSpreadsheet.class,aSheetsIA.getByIndex(0));
-					
-			//Object sheet = xSpread.getByName("MySheet");
-			//XSpreadsheet xSpreadsheet =	(XSpreadsheet)UnoRuntime.queryInterface(XSpreadsheet.class, sheet);
+
 			XCell xCell = maSheet.getCellByPosition(0, 0);
-			xCell.setValue(21);
+			xCell.setValue(22);
 		} catch (Exception ex) {
 			System.out.println("Exception" + ex.getMessage());
 		}
-		//System.out.println("names" + names.toString());
-		
-		// XTextRange xTextRange = xSpread.getElementNames();
-		// xTextRange.setString("Hello CSB (in Java)");
-		//
-		//
-		// // getting the text document object
-		// XSpreadsheetDocument xsheetdocument = (XSpreadsheetDocument)
-		// UnoRuntime
-		// .queryInterface(XSpreadsheetDocument.class, xSc.getDocument());
-		// // XSpreadsheets xSpread = xsheetdocument.getSheets();
-		// // String[] names = xSpread.getElementNames();
-		// // xSpread.insertNewByName("MySheet", (short) 0);
-		// // try {
-		// // Object sheet = xSpread.getByName("MySheet");
-		// // XSpreadsheet xSpreadsheet =
-		// (XSpreadsheet)UnoRuntime.queryInterface(
-		// // XSpreadsheet.class, sheet);
-		// //
-		// // XCell xCell = xSpreadsheet.getCellByPosition(0, 0);
-		// // xCell.setValue(21);
-		// // XIndexAccess aSheetsIA = UnoRuntime.queryInterface(
-		// // XIndexAccess.class, xSpread);
-		// // maSheet = UnoRuntime.queryInterface(XSpreadsheet.class,
-		// // aSheetsIA.getByIndex(0));
-		// // // enter some values in B3:B11
-		// // for (int iCounter = 1; iCounter < 10; iCounter++) {
-		// // XCell aCell = maSheet.getCellByPosition(1, 1 + iCounter);
-		// // aCell.setValue(iCounter);
-		// // }
-		//
-		// // } catch (Exception ex) {
-		// // System.out.println("Exception" + ex.getMessage());
-		// // }
-		// // System.out.println("names" + names.toString());
-		// // XTextRange xTextRange = xSpread.getElementNames();
-		// // xTextRange.setString("Hello CSB (in Java)");
 
 	}
 
