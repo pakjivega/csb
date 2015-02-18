f16a7fdba6132e9e3f64ffecbbbe3c5ebe891eb9
Jorge de la Vega <pakjivega@gmail.com>
2015-02-18 00:47:28 +0000

    Second Line Implemented. Ordenante.

--- src/main/java/com/pakjivega/libreoffice/csb/CSB.java
+++ src/main/java/com/pakjivega/libreoffice/csb/CSB.java
@@ -16,6 +16,8 @@ public abstract class CSB {
 	private static File csbFile = new File(nameFile);
 	private static final String PRESA1 = "51";
 	private static final String PRESA2 = "80";
+	private static final String ORDEA1 = "53";
+	private static final String ORDEA2 = "80";
 	
 	public static String generateFile(XSpreadsheet maSheet, String cadenaa) {
 		String response = null;
@@ -25,15 +27,9 @@ public abstract class CSB {
 			FileWriter fw = new FileWriter(csbFile);
 			fw.write(getCabeceraPresentador(maSheet));
 			fw.write(System.lineSeparator());
-			fw.write(secondLine);
+			fw.write(getCabeceraOrdenante(maSheet));
 			fw.write(System.lineSeparator());
-			Date now = new Date();
-			fw.write("now:" + now);
-			fw.write(System.lineSeparator());
-			fw.write("now.getDate():" + now.getDate());
-			fw.write(System.lineSeparator());
-			fw.write("now.getMonth():" + now.getMonth());
-			fw.write(System.lineSeparator());
+			
 			fw.close();
 		} catch (Exception ex) {
 			// do stuff with exception
@@ -59,6 +55,30 @@ public abstract class CSB {
 			ex.printStackTrace();
 		}
 		return firstLine;
+	}
+	private static String getCabeceraOrdenante(XSpreadsheet maSheet) {
+		String secondLine = null;
+		try {
+			Date now = new Date();
+			SimpleDateFormat sdf = new SimpleDateFormat("ddMMyy");
+			SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+			Date dateCargo = dateFormat.parse(maSheet.getCellByPosition(1, 5).getFormula());
+			String sDate= sdf.format(now);
+			String NIF = maSheet.getCellByPosition(1, 2).getFormula();
+			String NamePresent = maSheet.getCellByPosition(1, 1).getFormula();
+			String sDateCargo = sdf.format(dateCargo);
+	
+			secondLine = ORDEA1 + ORDEA2 + NIF + "000000000000".substring( NIF.length()) + sDate + sDateCargo + 
+					NamePresent + "                                        ".substring( NamePresent.length()) +
+					maSheet.getCellByPosition(1, 4).getFormula() + maSheet.getCellByPosition(2, 4).getFormula() + 
+					maSheet.getCellByPosition(3, 4).getFormula() + maSheet.getCellByPosition(4, 4).getFormula() +
+					"        " + "01";
+		} catch (Exception ex) {
+			// do stuff with exception
+			// response = ex.getMessage();
+			ex.printStackTrace();
+		}
+		return secondLine;
 	}
 
 }
